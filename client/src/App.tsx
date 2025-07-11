b19c7cebfe396b6bc585b659160121428ea7571c
surajinventurs <suraj.inventurs@gmail.com>
2025-07-11 09:57:13 +0530

    bug fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,22 +6,23 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+function RouterSetup() {
   return (
     <Switch>
-      <Route path="/universityvision/" component={Home} />
+      <Route path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
 }
-
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <Router base="/universityvision">
+          <RouterSetup />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
